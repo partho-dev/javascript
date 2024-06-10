cf7a6dcdf46c7d3eb11d10a3cc2b3a14a1e66794
Partho_Mac_15 <macbook@MacBooks-MacBook-Pro.local>
2024-06-10 22:55:56 +0530

    Understand Loop

---
+++ jsConcept-Loop.js
@@ -0,0 +1,29 @@
+// Loops
+
+// 1. For loop Dry Run 
+// for(start; end; change) {}
+
+
+// 2. While Loop Dry run 
+/**
+ start 
+ while( end ){
+    change
+ }
+ * 
+ */
+
+// example for for 
+// run the loop for 5 times 
+for (let num = 1; num <=5; num ++) {
+    console.log(`${num}: This is For Loop ${num} line`)
+}
+
+// example for while Loop 
+// run the loop for 5 times 
+
+let i=1;
+while(i<=5){
+    console.log(`${i}: This is While Loop ${i} line`)
+    i++
+}
